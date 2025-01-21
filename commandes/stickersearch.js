febdf971827a6cbb1f455119629bc77f752914af
CYBER GHOST <chinthakashamin0@gmail.com>
2025-01-21 19:04:50 +0530

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'DEXTER-MD',
+      author: 'GHOST-MD',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
